e1dcb44c3aeea8df5b6d45f0259b722735359646
Himanshu Malviya <himanshumalviya1500@gmail.com>
2024-08-10 10:34:06 +0530

    feat: Implement login page with authentication flow

---
+++ frontend/src/logIn/Login.js
@@ -0,0 +1,102 @@
+import { Form, Input, Button } from "antd";
+import React, { useEffect } from "react";
+import { Row, Col } from "antd";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../actions/authActions";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+function Login(props) {
+  const navigate = useNavigate();
+  const { isLoading } = props;
+  
+  useEffect(() => {
+    if (props.isAuthenticated) {
+      navigate("/");
+    }
+  }, [props]);
+
+  const submitForm = (values) => {
+    props.sendLoginRequest(values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <>
+      <Row justify="center" align="middle" className="hero">
+        <Col xs={22} sm={22} md={6} lg={6} className="signin__container">
+          <Form
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={submitForm}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                },
+              ]}
+            >
+              <Input placeholder="Email" />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password placeholder="Password" />
+            </Form.Item>
+            <div
+              className="link"
+              style={{
+                textAlign: "center",
+                fontWeight: 500,
+                marginBottom: "15px",
+              }}
+            >
+              <Link to="/signup">New here? Create your account.</Link>
+            </div>
+            <Form.Item>
+              <Button
+                type="primary"
+                className="sign__in"
+                style={{ minWidth: "44px" }}
+                loading={isLoading}
+                htmlType="submit"
+              >
+                {!isLoading ? "Sign In" : "Logging In"}
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.isAuthenticated,
+    isLoading: state.auth.isLoading,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    sendLoginRequest: (values) => dispatch(loginUser(values)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
