044818ce68bc81505a5f2628036af80f86334235
Himanshu Malviya <himanshumalviya1500@gmail.com>
2024-08-10 12:08:34 +0530

    feat: Added basic backend boilerplate

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const user = require("./routes/user");
+const teacher = require("./routes/teacher");
+const student = require("./routes/student");
+const InitiateMongoServer = require("./config/db");
+const cookieParser = require("cookie-parser");
+const cors = require('cors');
+var path = require("path");
+require('dotenv').config()
+
+InitiateMongoServer();
+
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use("/teacher", teacher);
+app.use("/student", student);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
