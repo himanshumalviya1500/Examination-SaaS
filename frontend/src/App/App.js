a99e4ced30be40342a2f57b45b10fdd4ebb6eb2c
Himanshu Malviya <himanshumalviya1500@gmail.com>
2024-08-12 20:37:50 +0530

    feat: corrected protected route path

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ProtectedRoute from "../ProtectedRoute";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 import StudentDashboard from "../Pages/Dashboard/Dashboard";
 import TeacherDashboard from "../Pages/Teacher/Dashboard/Dashboard";
 import AttemptTest from "../Pages/AttemptTest/AttemptTest";
