94499185746af396a5b11e0be61e34157f877987
Himanshu Malviya <himanshumalviya1500@gmail.com>
2024-08-12 17:48:20 +0530

    feat: fixed store folder location for build failed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App/App";
 import { Provider } from "react-redux";
-import store from "./Store";
+import store from "./store/index";
 
 ReactDOM.render(
   <React.StrictMode>
